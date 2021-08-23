92ef8c67903429f2aaa0e36ce461afb18263f4d6
Pasha <pashdoctor@gmail.com>
2021-08-23 14:06:45 -0400

    changed file name

---
+++ index.js
@@ -0,0 +1,13 @@
+// const headerOne = document.querySelectorAll('h1')
+
+// headerOne.style.color = 'blue';
+
+// for (let i = 0; i < headerOne.length i++) {
+//     console.log(headerOne[i]);
+// }
+
+const headerOne = document.querySelectorAll('h1');
+
+for (let i = 0; i < headerOne.length; i++) {
+    headerOne[i].style.color = "blue";
+}
